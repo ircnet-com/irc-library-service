2a7309f2c16ea1610cfe7e6836c038866838af72
Patrick <patrick@ircnet.com>
2022-01-13 15:54:03 +0100

    Preparation for IRCv3 message tags

--- src/main/java/com/ircnet/library/service/event/SQueryEvent.java
+++ src/main/java/com/ircnet/library/service/event/SQueryEvent.java
@@ -3,12 +3,15 @@ package com.ircnet.library.service.event;
 import com.ircnet.library.common.User;
 import com.ircnet.library.service.connection.IRCServiceConnection;
 
+import java.util.Map;
+
 public class SQueryEvent extends AbstractServiceEvent {
     private User from;
     private String message;
 
-    public SQueryEvent(IRCServiceConnection ircConnection, User from, String message) {
+    public SQueryEvent(IRCServiceConnection ircConnection, Map<String, String> messageTags, User from, String message) {
         this.ircConnection = ircConnection;
+        this.messageTags = messageTags;
         this.from = from;
         this.message = message;
     }
