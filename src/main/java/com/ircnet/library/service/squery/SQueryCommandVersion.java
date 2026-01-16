8046020642c7681b01ac669005194c803a8bdcba
Patrick <patrick@ircnet.com>
2026-01-16 11:20:15 +0100

    Revert "Corrected version reply"
    This reverts commit 93377dfd27f64ac59c0a4fd1d11bb74fc43020fd.

--- src/main/java/com/ircnet/library/service/squery/SQueryCommandVersion.java
+++ src/main/java/com/ircnet/library/service/squery/SQueryCommandVersion.java
@@ -40,7 +40,8 @@ public class SQueryCommandVersion extends SQueryCommand<IRCConnectionService> {
     @Override
     public void processCommand(IRCServiceConnection ircServiceConnection, User from,
                                String message, Map<String, String> tags) {
-        ircConnectionService.notice(ircServiceConnection, from.getNick(), "%s", appVersionProvider.get());
+        ircConnectionService.notice(ircServiceConnection, from.getNick(), "%s v%s",
+                properties.getName(), appVersionProvider.get());
     }
 
     /**
